38d1ec45cfd5fbac28783560ed0b3b63d54e1870
nozza <znozadze@affix.pro>
2021-04-20 17:02:20 +0400

    first commit

---
+++ src/Components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MasterLayout from "../Layout";
+
+const Index = () => {
+    return (
+        <MasterLayout>
+            
+        </MasterLayout>
+    );
+};
+
+export default Index;
---
+++ src/Components/Layout/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from "styled-components";
+
+const Index = () => {
+    return (
+        <div>
+           hello
+        </div>
+    );
+};
+
+export default Index;
